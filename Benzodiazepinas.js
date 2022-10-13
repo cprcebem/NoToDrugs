c64dbdedbf85c073eb142ba7c6dc88317ac4d4e1
NoTengoUnUsuario <pabloblgra@gmail.com>
2022-10-13 12:50:57 +0200

    casi finalizado

--- Benzodiazepinas.js
+++ Benzodiazepinas.js
@@ -1,9 +1,6 @@
 // Para que lo puedas usar en tu index.html
 let title = "Benzodiazepinas";
 
-let info =
-  "Tipo de medicamento que se usa para aliviar la ansiedad y el insomnio (dificultad para dormir). Las benzodiacepinas también se usan para relajar los músculos y prevenir crisis convulsivas. Aumentan el efecto de una sustancia química del cerebro llamada GABA, que es un neurotransmisor (sustancia que usan los nervios para enviarse mensajes entre sí). Esto hace que la actividad nerviosa se haga más lenta. Las benzodiacepinas son un tipo de depresor del SNC.";
-
 let type = "Psicotrópico";
 
 let legal = true;
@@ -14,6 +11,6 @@ let multa = [
   },
   { otherCountries: "idk" },
 ];
-let problem = [];
+let problem = ["muxo texto"];
 
 console.log("En España: " + legal);
